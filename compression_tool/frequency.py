5466dab83bbf271b8101dc45b42d12784259ed2f
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-10-13 22:00:51 +0300

    Refactor count_frequencies function to use Counter for improved performance.

--- compression_tool/frequency.py
+++ compression_tool/frequency.py
@@ -1,11 +1,5 @@
+from collections import Counter
 
 def count_frequencies(data):
-    """Counts the frequency of each byte in the given data."""
-    freq = {}
-    for byte in data:
-        if byte in freq:
-            freq[byte] += 1
-        else:
-            freq[byte] = 1
-    return freq
+    return Counter(data)
 
