9d5cdc1c3d86ef79619bb8d7f316976f03e9ea34
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-10-13 21:59:27 +0300

    Refactor count_frequencies function to dynamically count byte frequencies from input data

--- compression_tool/frequency.py
+++ compression_tool/frequency.py
@@ -1,3 +1,11 @@
 
 def count_frequencies(data):
-    return {97: 3, 98: 2, 99: 1}
+    """Counts the frequency of each byte in the given data."""
+    freq = {}
+    for byte in data:
+        if byte in freq:
+            freq[byte] += 1
+        else:
+            freq[byte] = 1
+    return freq
+
