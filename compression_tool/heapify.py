341599cbd91caacfd115d75593c1aaf74d7d5a6b
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-10-23 23:22:02 +0300

    Implement build_min_heap function and add test for check_min_heap_valid

--- compression_tool/heapify.py
+++ compression_tool/heapify.py
@@ -1,15 +1,17 @@
 
-def build_min_heap(data):
-    return data
+def build_min_heap(heap):
+    for i in range((len(heap)//2) -1, -1, -1): # start from last non-leaf node, stop at idx 0, step -1 meaning negative steps.
+        sift_down(heap, i)
+    return heap
 
 def sift_down(heap, i):
-    n = len(heap)
-    left_index = i*2 + 1
-    right_index = i*2 + 2
+    n = len(heap) 
+    left_index = i*2 + 1 
+    right_index = i*2 + 2 
 
-    smallest = i
+    smallest = i 
 
-    if left_index < n and heap[left_index][0] < heap[smallest][0]:
+    if left_index < n and heap[left_index][0] < heap[smallest][0]: 
         smallest = left_index
     if right_index < n and heap[right_index][0] < heap[smallest][0]:
         smallest = right_index
@@ -17,3 +19,6 @@ def sift_down(heap, i):
     if smallest != i:
         heap[i], heap[smallest] = heap[smallest], heap[i]
         sift_down(heap, smallest)
+
+def check_min_heap_valid(heap):
+    pass
--- tests/test_heapify.py
+++ tests/test_heapify.py
@@ -1,4 +1,4 @@
-from compression_tool.heapify import build_min_heap, sift_down
+from compression_tool.heapify import build_min_heap, sift_down, check_min_heap_valid
 import pytest
 
 def test_heapify_simple():
@@ -29,3 +29,14 @@ def test_sift_down():
 
     assert data[0][0] == 1
 
+def test_check_min_heap_valid():
+    data = [
+        (2, b'a'),
+        (3, b'b'),
+        (4, b'c'),
+        (5, b'd'),
+        (6, b'e'),
+        (7, b'f'),
+    ]
+
+    assert check_min_heap_valid(data) is True
