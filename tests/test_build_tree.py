6723013ee05c2a34c709894d106f6c289909493a
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-03 21:22:08 +0200

    Implement initial tests for build_tree function

---
+++ tests/test_build_tree.py
@@ -0,0 +1,20 @@
+from compression_tool.build_tree import build_tree
+from compression_tool.tree import LeafNode, InternalNode
+
+def test_empty_tree():
+    assert build_tree({}) == None
+
+def test_single_symbol():
+    root = build_tree({97: 5})
+    assert isinstance(root, LeafNode)
+    assert root.symbol == 97 and root.weight == 5
+
+def test_two_symbols():
+    root = build_tree({97: 3, 98: 2})
+    assert isinstance(root, InternalNode)
+    assert root.weight == 5
+    assert {root.left.weight, root.right.weight} == {3,2} 
+
+def test_small_multi_set():
+    root = build_tree({97: 5, 98: 2, 99: 1})
+    assert root.weight == 8
