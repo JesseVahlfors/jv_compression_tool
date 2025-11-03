53ea0cc8ef8afed33b25cdeff07b60fad5bd3fb1
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-03 21:08:58 +0200

    Add node repr and comparison tests

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -20,3 +20,19 @@ def test_internalnode_initialization():
     assert left == left
     assert right == right
     assert internal.is_leaf == False
+
+def test_node_comparison():
+    n1 = Node(weight=2)
+    n2 = Node(weight=5)
+
+    assert n1 < n2
+
+def test_repr_on_nodes():
+    node1 = Node(weight=7)
+    l = LeafNode(symbol = 97, weight = 3)
+    r = LeafNode(symbol = 80, weight = 5)
+    internal_node = InternalNode(l, r)
+
+    assert repr(node1) == "Internal(weight=7)"
+    assert repr(l) == "Leaf(symbol=97, weight=3)"
+    assert repr(internal_node) == "Internal(weight=8)"
