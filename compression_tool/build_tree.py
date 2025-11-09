7b9177081c1984fccc8676a68f1456eb9d304739
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-09 21:42:24 +0200

    Feat(build tree): heap based  tree construction for >=2 symbols

--- compression_tool/build_tree.py
+++ compression_tool/build_tree.py
@@ -1,15 +1,28 @@
 from compression_tool.tree import Node, LeafNode, InternalNode
-from compression_tool.utils.heapify import build_min_heap
+from compression_tool.utils.heapify import build_min_heap, heap_push, pop_min, comes_before
 # Convert freq → list of LeafNodes → heapify → loop merge.
 
 def build_tree(freq: dict[int, int]) -> Node | None:
-
-    if freq == {}:
+    if not freq:
         return None
+    if len(freq) == 1:
+        (sym, wt), = freq.items()
+        return LeafNode(symbol=sym,weight=wt)
     
-    heap = [LeafNode(symbol, weight) for symbol, weight in freq.items()]
+    heap = []
+    for sym, wt in freq.items():
+        heap_push(heap, LeafNode(symbol=sym, weight=wt))
 
-    heap = build_min_heap(heap)
+    build_min_heap(heap)
+
+    while len(heap) > 1:
+        first = pop_min(heap)
+        second = pop_min(heap)
+
+        left, right = (first, second) if comes_before(first, second) else (second, first)
+        parent = InternalNode(left=left, right=right)
+
+        heap_push(heap, parent)
 
     return heap[0]
 
