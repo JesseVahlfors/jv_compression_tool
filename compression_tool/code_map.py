93f1196ce249d94c72bdd0ea3279f30098bb0ade
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-10 23:47:45 +0200

    Feat(code_map): implement code builder function to traverse the huffman tree recursively

--- compression_tool/code_map.py
+++ compression_tool/code_map.py
@@ -1,8 +1,19 @@
+from compression_tool.tree import Node, InternalNode
 
-def code_map(Node) -> dict:
-    if Node == None:
+
+def build_code_map(node: Node) -> dict:
+    if node == None:
         return {}
+    map = {}
+    prefix=''
+
+    def code_builder(node: Node, prefix: str, map: dict):    
+        if node.is_leaf:
+            map[node.symbol] = prefix
+        if isinstance(node, InternalNode):
+            code_builder(node.left, prefix + "0", map)
+            code_builder(node.right, prefix + "1", map)
     
-    code_map = {}
-    code_map[Node.symbol] = 0
-    return code_map
+    code_builder(node, prefix, map)
+    
+    return map
