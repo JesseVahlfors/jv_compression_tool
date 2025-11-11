49fdc1503b2dd7e342bfa60ddd89ef8f4c87c985
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-11 23:56:19 +0200

    Test(header): Add happy path i/o test with desired input and output.

---
+++ tests/test_header.py
@@ -0,0 +1,15 @@
+import pytest
+from compression_tool.header import build_header, decode_header
+
+def test_header_io():
+    pad_len = 3
+    freq = {97: 4, 98: 5, 99: 1}
+    codestring = "101001101"
+
+    header = build_header(pad_len, freq)
+    parsed = decode_header(header + codestring)
+
+    assert parsed.version == "HUF1"
+    assert parsed.pad_len == 3
+    assert parsed.freq == {97: 4, 98: 5, 99: 1}
+    assert parsed.payload == codestring
