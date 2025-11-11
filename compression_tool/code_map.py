f668aeab90832fa950c3712bde34384b5c562105
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-11 22:17:01 +0200

    Feat(code_map): add type validation to code map function

--- compression_tool/code_map.py
+++ compression_tool/code_map.py
@@ -4,6 +4,8 @@ from compression_tool.tree import Node, InternalNode
 def build_code_map(node: Node) -> dict:
     if node == None:
         return {}
+    if not isinstance(node, Node):
+        raise TypeError(f"root is not a Node instance: {node!r} ")
     map = {}
     prefix=''
 
