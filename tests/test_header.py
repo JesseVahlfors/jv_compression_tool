28fab3902e35ee747a8b58c54a8b38bb7959428e
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-12 00:18:23 +0200

    Test(header): refactor string assert to own function "test_build_header_string"

--- tests/test_header.py
+++ tests/test_header.py
@@ -13,4 +13,11 @@ def test_header_io():
     assert parsed.pad_len == 3
     assert parsed.freq == {97: 4, 98: 5, 99: 1}
     assert parsed.payload == codestring
+
+def test_build_header_string():
+    pad_len = 3
+    freq = {97: 4, 98: 5, 99: 1}
+
+    header = build_header(pad_len, freq)
+
     assert header == "HUF1|pad=3|freq=97:4,98:5,99:1|"
