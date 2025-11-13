debcb15e1ff7b985d413e3481f0870aac8d8b673
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-13 23:38:40 +0200

    Test(header): implement a test for decode_header function. checks version and payload. changed freq dict order on build_header string to test also header  frequency string sorting.

--- tests/test_header.py
+++ tests/test_header.py
@@ -16,8 +16,19 @@ def test_header_io():
 
 def test_build_header_string():
     pad_len = 3
-    freq = {97: 4, 98: 5, 99: 1}
+    freq = {98: 5, 97: 4,  99: 1}
 
     header = build_header(pad_len, freq)
 
     assert header == "HUF1|pad=3|freq=97:4,98:5,99:1|"
+
+def test_decode_header_version_string():
+    pad_len = 3
+    freq = {97: 4, 98: 5, 99: 1}
+    codestring = "101001101"
+
+    header = build_header(pad_len, freq)
+    parsed = decode_header(header + codestring)
+
+    assert parsed.version == "HUF1"
+    assert parsed.payload == codestring
