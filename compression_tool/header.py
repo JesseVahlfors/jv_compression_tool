dbc868eb6f0c7070088c40448c8b75d6d52d3a7b
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-15 18:39:30 +0200

    Feat(header): Add version and freq validation. add valid return when freq dict is empty

--- compression_tool/header.py
+++ compression_tool/header.py
@@ -27,21 +27,37 @@ def decode_header(data: str)-> HeaderInfo:
     if data.rfind("|") == -1:
         raise ValueError("Unknown header format")
     
-    fields = data.split("|")
+    payload_idx = data.rfind("|")
+    header = data[:payload_idx]
+    payload = data[payload_idx +1 :]
 
+    fields = header.split("|")
+    if len(fields) < 3:
+        raise ValueError("Unknown header format")
+
+    #version
     version = fields[0]
+    if len(version) < 4: 
+         raise ValueError("Unknown version format")
+    
     name = version[:3]
-    version_num = int(version[3:])
+    suffix = version[3:]
 
+    if not suffix.isdigit():
+        raise ValueError("Unknown version format")
+    
+    version_num = int(suffix)
+    if version_num < 0:
+        raise ValueError("Version number must be positive")
     if name != NAME:
         raise ValueError("Unknown header format")
     if version_num > VERSION:
         raise ValueError("Unsupported newer version")
-
+    
+    #pad_len
     pad_field = fields[1]
     if not pad_field.startswith("pad="):
         raise ValueError("pad field missing or malformed")
-    
     pad_value = pad_field.split("=")[1]
     if pad_value == '':
         raise ValueError(f"pad length missing.")
@@ -49,17 +65,27 @@ def decode_header(data: str)-> HeaderInfo:
     if 0 > pad_len or pad_len > 7:
         raise ValueError(f"pad length out of range: {pad_len}")
 
+    #freq
     freq_string_field = fields[2]
+    freq = {}
     if not freq_string_field.startswith("freq="):
         raise ValueError("freq field missing or malformed")
     freq_string = freq_string_field.split("=")[1]
+    if freq_string == "":
+        return HeaderInfo(version, pad_len, freq, payload)
     freq_string_list = freq_string.split(',')
-    freq = {}
     for val in freq_string_list:
         val = val.split(':')
-        freq[int(val[0])] = int(val[1])
+        if len(val) != 2:
+            raise ValueError("symbol or weight missing")
+        symbol = int(val[0])
+        weight = int(val[1])
+        if 255 < symbol or symbol < 0 or weight < 1:
+            raise ValueError(f"freq symbol out of range (0-255) or weight is less than 1")
+        if symbol in freq:
+            raise ValueError(f"Symbol duplicate in freq dict")
+        freq[symbol] = int(weight)
 
-    payload = fields[3]
 
     return HeaderInfo(version, pad_len, freq, payload)
     
