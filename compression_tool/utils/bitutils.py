7f9b1f0be73068cce3b9773a415abcbd901da404
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-18 23:31:38 +0200

    feat(bitutils): implement pack_bits function.

--- compression_tool/utils/bitutils.py
+++ compression_tool/utils/bitutils.py
@@ -1,6 +1,26 @@
 
 def pack_bits(bits: str) -> tuple[bytes, int]:
-    pass
+    if bits == "":
+        return (b"", 0)
+    
+    n = len(bits)
+    if n % 8 == 0:
+        pad_len = 0
+    else:
+        pad_len = 8 - (n % 8)
+    padded_bits = bits + "0" * pad_len
+    chunks = [padded_bits[i: i + 8] for i in range(0,len(padded_bits), 8)]
+
+    value_list = []
+    for chunk in chunks:
+        value = 0
+        for bit in chunk:
+            value <<= 1
+            value += int(bit)
+        value_list.append(value)
+            
+    return bytes(value_list), pad_len
+    
 
 def unpack_bits(data: bytes, pad_len: int) -> str:
     pass
