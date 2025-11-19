feecaac6b3a8b9665202e30b5c0b602b89915fa6
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-19 23:23:43 +0200

    test(bitutils): add tests for unpack_bits function.

--- tests/test_bitutils.py
+++ tests/test_bitutils.py
@@ -1,31 +1,31 @@
 import pytest
-from compression_tool.utils.bitutils import pack_bits
+from compression_tool.utils.bitutils import pack_bits, unpack_bits
 
 def test_pack_bits_empty_string():
-    bytes, pad_len = pack_bits("")
+    bits, pad_len = pack_bits("")
 
-    assert bytes == b""
+    assert bits == b""
     assert pad_len == 0
 
 def test_pack_bits_length_multiple_of_8():
     string = "01000001"
-    bytes, pad_len = pack_bits(string)
+    bits, pad_len = pack_bits(string)
 
-    assert bytes == b"A"
+    assert bits == b"A"
     assert pad_len == 0
 
 def test_pack_bits_length_not_multiple_of_8():
     string = "010"
-    bytes, pad_len = pack_bits(string)
+    bits, pad_len = pack_bits(string)
 
-    assert bytes == b"@"
+    assert bits == b"@"
     assert pad_len == 5
 
 def test_pack_bits_multi_byte_string():
     string = "0100000101000010"
-    bytes, pad_len = pack_bits(string)
+    bits, pad_len = pack_bits(string)
 
-    assert bytes == b"AB"
+    assert bits == b"AB"
     assert pad_len == 0
 
 @pytest.mark.parametrize("invalids", [
@@ -38,3 +38,31 @@ def test_pack_bits_multi_byte_string():
 def test_pack_bits_invalids_input(invalids):
     with pytest.raises(ValueError):
         pack_bits(invalids)
+
+def test_unpack_bits_empty_byte():
+    data =  b""
+    pad_len = 0
+    bits = unpack_bits(data, pad_len)
+
+    assert bits == ""
+
+def test_unpack_bits_one_byte():
+    data = b"A"
+    pad_len = 0
+    bits = unpack_bits(data, pad_len)
+
+    assert bits == "01000001"
+
+def test_unpack_bits_byte_not_multiple_of_8():
+    data = b"@"
+    pad_len = 5
+    bits = unpack_bits(data, pad_len)
+
+    assert bits == "010"
+
+def test_unpack_bits_multi_byte_string():
+    data = b"AB"
+    pad_len = 0
+    bits = unpack_bits(data, pad_len)
+
+    assert bits == "0100000101000010"
