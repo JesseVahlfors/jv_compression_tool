e1818fecfdd0bc56e3e61f1b9c6a41b12e662e27
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-20 23:08:29 +0200

    restructured __init__ imports

--- compression_tool/___init__.py
+++ compression_tool/___init__.py
@@ -1,14 +1,10 @@
-from .frequency import build_frequency_table
-from .build_tree import build_tree
-from .code_map import build_code_map
-from .header import build_header
-from .utils.bitutils import pack_bits, unpack_bits
+from .compressor import compress
+from .decompressor import decompress
+from .file_compressor import compress_file, decompress_file
 
 __all__ = [
-    "build_frequency_table",
-    "build_tree",
-    "build_code_map",
-    "build_header",
-    "pack_bits",
-    "unpack_bits",
+    "compress",
+    "decompress",
+    "compress_file",
+    "decompress_file",
 ]
