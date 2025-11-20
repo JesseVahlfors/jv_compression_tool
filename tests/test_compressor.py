2b122752db367c7749fb09a13c2b264a181caf08
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-20 23:06:15 +0200

    test(compressor): add tests empty_data and single symbol

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -4,7 +4,13 @@ from compression_tool.utils.bitutils import pack_bits
 from compression_tool.header import build_header
 
 def test_compress_empty_data():
-    freq = {}
-    bitstring = ""
-    packed_bits, pad_len = pack_bits("")
-    header = build_header(0, {})
+    data = compress(b"")
+    print(data)
+    assert data == build_header(0, {})
+
+def test_compress_single_symbol():
+    result = compress(b"A")
+    header_str = build_header(7, {65:1})
+    expected_header = header_str.encode("utf-8")
+    expected_body = b"\x00"
+    assert result == expected_header + expected_body
