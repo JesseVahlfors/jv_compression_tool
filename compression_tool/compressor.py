6c6b76c0c5b19dccf40166ed0903e3bfa5baebea
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-20 23:07:06 +0200

    feat(compressor): add compress implementation to pass tests

--- compression_tool/compressor.py
+++ compression_tool/compressor.py
@@ -3,10 +3,25 @@
 # use pack_bits to turn codestring into bytes and get pad_len-> 
 # use pad len and freq table to make header -> 
 # return header + body bytes
+from compression_tool.header import build_header
+from compression_tool.frequency import build_frequency_table
+from compression_tool.build_tree import build_tree
+from compression_tool.code_map import build_code_map
+from compression_tool.utils.bitutils import pack_bits
 
-from compression_tool import build_frequency_table, pack_bits, build_tree, build_code_map, build_header
 
 def compress(data: bytes) -> bytes:
-    pass
+    if data == b"":
+        return build_header(0, {})
+    
+    freq_table = build_frequency_table(data)
+    root = build_tree(freq_table)
+    code_map = build_code_map(root)
+    codestring = ""
+    for symbol in data:
+        codestring += code_map[symbol]
+    packed_bytes, pad_len = pack_bits(codestring)
+    header = build_header(pad_len, freq_table)
+    header_bytes = header.encode("utf-8")
 
-
+    return header_bytes + packed_bytes
