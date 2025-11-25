c2eebbe2481a640b7c6b946f395b4c3500431793
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-11-25 22:40:17 +0200

    feat(init): rename compression and decompression imports for clarity

--- compression_tool/___init__.py
+++ compression_tool/___init__.py
@@ -1,10 +1,13 @@
-from .compressor import compress
-from .decompressor import decompress
-from .file_compressor import compress_file, decompress_file
+from .compressor import compress as compress_bytes
+from .decompressor import decompress as decompress_bytes
+from .file_compressor import compress_file, CompressionResult
+from .file_decompressor import decompress_file, DecompressionResult
 
 __all__ = [
-    "compress",
-    "decompress",
+    "compress_bytes",
+    "decompress_bytes",
     "compress_file",
     "decompress_file",
+    "CompressionResult",
+    "DecompressionResult",
 ]
